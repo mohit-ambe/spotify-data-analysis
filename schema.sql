670c8e063e3b083e51337dd401e63bb0a21955d6
Mohit Ambe <mambe@purdue.edu>
2026-03-10 19:51:01 -0400

    Initial DB Schema

---
+++ schema.sql
@@ -0,0 +1,52 @@
+CREATE TABLE Artists (
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255),
+    artist_url VARCHAR(255)
+);
+
+CREATE TABLE Albums (
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255),
+    album_type VARCHAR(50),
+    release_date VARCHAR(20),
+    total_tracks INT,
+    image_url VARCHAR(255),
+    album_url VARCHAR(255)
+);
+
+CREATE TABLE AlbumArtists (
+    album_id VARCHAR(255),
+    artist_id VARCHAR(255),
+    artist_order INT,
+    PRIMARY KEY (album_id, artist_id),
+    FOREIGN KEY (album_id) REFERENCES Albums(id),
+    FOREIGN KEY (artist_id) REFERENCES Artists(id)
+);
+
+CREATE TABLE Tracks (
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255),
+    album_id VARCHAR(255),
+    disc_number INT,
+    duration_ms INT,
+    explicit BOOLEAN,
+    track_number INT,
+    track_url VARCHAR(255),
+    FOREIGN KEY (album_id) REFERENCES Albums(id)
+);
+
+CREATE TABLE TrackArtists (
+    track_id VARCHAR(255),
+    artist_id VARCHAR(255),
+    artist_order INT,
+    PRIMARY KEY (track_id, artist_id),
+    FOREIGN KEY (track_id) REFERENCES Tracks(id),
+    FOREIGN KEY (artist_id) REFERENCES Artists(id)
+);
+
+CREATE TABLE TrackEvents (
+    track_id VARCHAR(255),
+    played_at DATETIME,
+    PRIMARY KEY (track_id, played_at),
+    FOREIGN KEY (track_id) REFERENCES Tracks(id)
+);
